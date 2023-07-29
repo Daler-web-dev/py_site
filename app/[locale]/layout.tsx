eb87c5434f589e15322518ce76cf6fa090a8798e
Daler <dsarifkulo021@gmail.com>
2023-07-30 02:45:35 +0500

    first commit

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,68 @@
+import { NextIntlClientProvider } from "next-intl";
+import "../globals.css";
+import {  Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+export function generateStaticParams() {
+	return [{ locale: "ru" }, { locale: "ru" }];
+}
+
+const inter = Montserrat({ subsets: ["latin"] });
+
+export default async function LocaleLayout({
+	children,
+	params: { locale },
+}: {
+	children: React.ReactNode;
+	params: any;
+}) {
+	let translation;
+	try {
+		translation = (await import(`../../langs/${locale}.json`)).default;
+	} catch (error) {
+		notFound();
+	}
+	
+
+	return (
+		<html lang="en">
+			<body
+				className={`${inter.className} max-w-[1440px] mx-auto my-0 max-h-[1020px] relative`}
+			>
+				<header className="flex justify-between items-center py-4  px-4 md:px-20 w-full absolute top-0">
+					<Image
+						src="/logo.svg"
+						alt=""
+						width="50"
+						height="50"
+						className="w-[50px] "
+					/>
+					<nav className="flex items-center gap-2">
+						<Link href="/uz" className={`text-base py-0 ${locale === "uz" ? "font-mediaum" : "font-extralight"}`}>
+							UZ
+						</Link>
+						<div className="h-[14px] w-[1px] bg-black"></div>
+						<Link href="/ru" className={`text-base py-0 ${locale === "ru" ? "font-mediaum" : "font-extralight"}`} >RU</Link>
+						<div className="h-[14px] w-[1px] bg-black"></div>
+						<Link
+							href="/en"
+							className={`text-base py-0 ${locale === "en" ? "font-mediaum" : "font-extralight"}`}
+						>
+							EN
+						</Link>
+					</nav>
+				</header>
+				<NextIntlClientProvider locale={locale} messages={translation}>
+				{children}
+				</NextIntlClientProvider>
+				<footer className="flex justify-between items-center py-4 px-4 md:px-20 w-full md:absolute bottom-0">
+					<span className="py-0 font-light opacity-50 text-sm ">
+						ⓒ 1991 - 2023, «PARVIZ YAKHYAYEV», ВСЕ ПРАВА ЗАЩИЩЕНЫ.
+					</span>
+				</footer>
+			</body>
+		</html>
+	);
+}
---
+++ app/[locale]/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Image from "next/image";
+import { BsInstagram, BsYoutube } from "react-icons/bs";
+import { FaTelegramPlane, FaFacebookF } from "react-icons/fa";
+import {useTranslations} from 'next-intl';
+import Link from "next/link";
+
+export default function Home() {
+	const t = useTranslations('Index')
+
+	return (
+		<main className="px-4 md:px-20 h-full md:h-screen flex flex-col-reverse gap-5 justify-end laptop:flex-row  laptop:justify-between items-center pt-[50px] laptop:pb-[0px] pb-[50px] overflow-hidden">
+			<div className="flex flex-col gap-6 items-start w-full">
+				<div>
+					<h2 className="tracking-wide mb-2">{t("fullName")}</h2>
+					<h1 className="md:text-5xl text-2xl font-medium">
+						{t("title")}
+					</h1>
+				</div>
+				<p className="md:w-[50ch] w-full">
+					{t("description")}
+				</p>
+				<div className="flex items-center gap-2">
+					<span className="py-0 font-light opacity-50">{t("socMedia")}:</span>
+					<nav className="flex items-center gap-6">
+						<Link href={""}>
+							<FaFacebookF size="20" color="black" />
+						</Link>
+						<Link href={""}>
+							<BsInstagram size="20" color="black" />
+						</Link>
+						<Link href={""}>
+							<Image
+								src="/vk.svg"
+								alt=""
+								width="30"
+								height="30"
+							/>
+						</Link>
+						<Link href={""}>
+							<BsYoutube size="20" color="black" />
+						</Link>
+						<Link href={""}>
+							<FaTelegramPlane size="20" color="black" />
+						</Link>
+					</nav>
+				</div>
+			</div>
+			<div
+				style={{
+					backgroundImage: `url("/person.png")`,
+					backgroundSize: "contain",
+					backgroundRepeat: "no-repeat",
+					backgroundPosition: "center",
+				}}
+				className="w-full h-[300px] tablet:h-[500px] laptop:h-screen"
+			></div>
+		</main>
+	);
+}
