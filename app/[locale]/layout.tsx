e3eee0b16fab5a57c389da49cb070ac4db368e87
Daler <dsarifkulo021@gmail.com>
2023-08-01 14:49:29 +0500

    favicon was added

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -27,6 +27,9 @@ export default async function LocaleLayout({
 
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="images/favicon.ico" />
+			</head>
 			<body className={inter.className}>
 				<header>
 					<Image
